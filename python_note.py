73e0846145b86007f9bee8c777e9af36e212ba89
Albastros <150889763+Albastros@users.noreply.github.com>
2024-02-02 14:03:12 -0800

    Add files via upload

---
+++ python_note.py
@@ -0,0 +1,59 @@
+# print("hello world")
+# age = 20
+# first_name = "John"
+# print(type(age))
+# print(type(first_name))
+
+
+# students_list = ['hanna', 'john', 'dav', 'sami', 'tom']
+# print(type(first_name)) # list data type 
+
+
+# print(first_name.count('n'))
+
+# # tuple
+# names = {'hanna', 'john', 'dav', 'sami', 'tom'}
+# print(type(names))
+# # set
+# students_list = {'hanna', 'john', 'dav', 'sami', 'tom'}
+# print(names)
+# student_info = {'name': 'hanna' ,'age':12, 'section': 'B' , 'height':1.3, 'skills': ['java','PHP', 'javascript']
+# }
+# print(student_info['height'])
+
+# #loop
+# for i in range(0,12):
+#     print (i)
+#     print ('hello')
+# for i in enumerate(students_list):
+#     print (i)   #value #index
+# # if else
+# age = 45
+# if age >18:
+#     print('adult')
+# elif (age < 18):
+#     print('under age')
+# else:
+#     print('error')
+#     # function
+# def greet():
+#     print ('hello')
+# def greet_with_name(name):
+#     print('hello' + name)
+# def greet_with_name(name):
+#     print(f'hello {name}')
+
+# def greet_with_name(name,age,last_name='kebede'):
+# # print (f'hello{name} age:{age} last name: {last_name}')
+
+# # greet ()
+# # greet_with_name('hanna',age =56)
+
+# # # greet ()
+# # # greet_with_name('hanna')
+
+# # def greet_with_name(*numbers):
+# #     for name in numbers:
+# #         print(f'hello {name}')
+# # greet_with_name('hannah','yyyy','yakob')
+
